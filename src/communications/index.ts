dae66636626fd2f791c8587602f846df24b50433
Fabien Lavocat <fabien.lavocat@dolby.com>
2022-11-02 10:50:07 -0700

    Remove recording API

--- src/communications/index.ts
+++ src/communications/index.ts
@@ -1,7 +1,6 @@
 import * as authentication from './authentication';
 import * as conference from './conference';
 import * as monitor from './monitor';
-import * as recording from './recording';
 import * as streaming from './streaming';
 
-export { authentication, conference, monitor, recording, streaming };
+export { authentication, conference, monitor, streaming };
