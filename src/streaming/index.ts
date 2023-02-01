d430eb82acf204f7c9a1b85e7f8265d6c60fd59f
Fabien Lavocat <fabien.lavocat@dolby.com>
2023-01-31 17:57:32 -0800

    Add Streaming Stream APIs

--- src/streaming/index.ts
+++ src/streaming/index.ts
@@ -2,6 +2,7 @@ import * as cluster from './cluster';
 import * as director from './director';
 import * as geo from './geo';
 import * as publishToken from './publishToken';
+import * as stream from './stream';
 import * as subscribeToken from './subscribeToken';
 
-export { cluster, director, geo, publishToken, subscribeToken };
+export { cluster, director, geo, publishToken, stream, subscribeToken };
