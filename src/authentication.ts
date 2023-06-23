7abe7942dde146f36ea231f26233bbd601739b9d
Fabien Lavocat <fabien.lavocat@dolby.com>
2023-06-23 13:37:01 -0700

    Update text description

--- src/authentication.ts
+++ src/authentication.ts
@@ -15,16 +15,16 @@ import JwtToken from './types/jwtToken';
  * @param expiresIn API token expiration time in seconds. If no value is specified, the default is 1800, indicating 30 minutes. The maximum value is 86,400, indicating 24 hours.
  * @param scope A list of case-sensitive strings allowing you to control what scope of access the API token should have. If not specified, the API token will possess unrestricted access to all resources and actions. The API supports the following scopes:
  * * `comms:client_access_token:create`: Allows requesting a client access token.
- * * `comms:conf:create`: Allows creating a new conference.
  * * `comms:conf:admin`: Allows administrating a conference, including actions such as Invite, Kick, Send Message, Set Spatial Listener's Audio, and Update Permissions.
+ * * `comms:conf:create`: Allows creating a new conference.
  * * `comms:conf:destroy`: Allows terminating a live conference.
  * * `comms:monitor:delete`: Allows deleting data from the Monitor API, for example, deleting recordings.
- * * `comms:monitor:read`: Allows reading data through the Monitor API.
  * * `comms:monitor:download`: Allows generating download URLs for data (e.g. recording) through the Monitor API.
- * * `comms:stream:write`: Allows starting and stopping RTMP or Real-Time streaming.
- * * `comms:remix:write`: Allows remixing recordings.
- * * `comms:remix:read`: Allows reading the remix status.
+ * * `comms:monitor:read`: Allows reading data through the Monitor API.
  * * `comms:record:write`: Allows starting and stopping recordings.
+ * * `comms:remix:read`: Allows reading the remix status.
+ * * `comms:remix:write`: Allows remixing recordings.
+ * * `comms:stream:write`: Allows starting and stopping RTMP or Real-Time streaming.
  * Incorrect values are omitted. If you want to give the token access to all Communications REST APIs, you can use a wildcard, such as comms:*
  *
  * @returns A {@link JwtToken} object through a {@link Promise}.
