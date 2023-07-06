795a6c9bc982c25e30d3b69f2b36632d10c1a14c
Fabien Lavocat <fabien.lavocat@dolby.com>
2023-07-06 16:00:32 -0700

    Remove deprecated APIs

--- src/communications/authentication.ts
+++ src/communications/authentication.ts
@@ -4,44 +4,6 @@ import JwtToken from '../types/jwtToken';
 import { GetClientAccessTokenOptions } from './types/authentication';
 
 /**
- * Gets a client access token to authenticate a session.
- *
- * @link https://docs.dolby.io/communications-apis/reference/get-client-access-token-v1
- *
- * @param appKey Your Dolby.io App Key.
- * @param appSecret Your Dolby.io App Secret.
- * @param expiresIn Access token expiration time in seconds. If no value is specified, the default is 3,600, indicating one hour. The maximum value is 86,400, indicating 24 hours.
- *
- * @returns A {@link JwtToken} object through a {@link Promise}.
- *
- * @deprecated This function is now deprecated and will be removed in the next release of this SDK. Please start using {@link getClientAccessTokenV2} instead.
- */
-export const getClientAccessToken = async (appKey: string, appSecret: string, expiresIn?: number): Promise<JwtToken> => {
-    const body = new URLSearchParams({
-        grant_type: 'client_credentials',
-    });
-    if (expiresIn) {
-        body.append('expires_in', expiresIn.toFixed(0));
-    }
-
-    const authz = Buffer.from(`${appKey}:${appSecret}`).toString('base64');
-
-    const options = {
-        hostname: Urls.getCommsSessionHostname(),
-        path: '/v1/oauth2/token',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cache-Control': 'no-cache',
-            Authorization: `Basic ${authz}`,
-        },
-        body: body.toString(),
-    };
-
-    const response = await sendPost(options);
-    return response as JwtToken;
-};
-
-/**
  * Generates a client access token.
  *
  * This API returns a client access token that your backend can request on behalf of a client to initialize the Dolby.io SDK in a secure way.
