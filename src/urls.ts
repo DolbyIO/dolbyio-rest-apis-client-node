36f6ff3b0289008024fc1c3e945b458f49562a00
Fabien Lavocat <fabien.lavocat@dolby.com>
2023-07-12 11:07:29 -0700

    Remove the Session URL

--- src/urls.ts
+++ src/urls.ts
@@ -1,7 +1,6 @@
 export interface Hostnames {
     api?: string;
     comms?: string;
-    commsSession?: string;
     rts?: string;
     rtsDirector?: string;
     mapi?: string;
@@ -10,7 +9,6 @@ export interface Hostnames {
 const hostnames: Hostnames = {
     api: 'api.dolby.io',
     comms: 'comms.api.dolby.io',
-    commsSession: 'session.voxeet.com',
 
     rts: 'api.millicast.com',
     rtsDirector: 'director.millicast.com',
@@ -28,10 +26,6 @@ export const getApiHostname = (): string => {
 
 export const getCommsHostname = (region?: string): string => {
     return region ? `${region}.${hostnames.comms}` : hostnames.comms;
-};
-
-export const getCommsSessionHostname = (): string => {
-    return hostnames.commsSession;
 };
 
 export const getRtsHostname = (): string => {
